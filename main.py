263042f7671d029a3993220774bfbd1cfc4d2b20
IIOrochiII <danielpostolowicz03@gmail.com>
2024-07-22 14:58:52 +0200

    Added copying of link using pyperclip in main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pyperclip
 import scrape
 
 if __name__ == "__main__":
-    name = input("Enter the name of channel like this(https://www.youtube.com/@{name})\n")
-    print(scrape.getrss(name))
+    name = input(
+        "Enter the name of channel like this(https://www.youtube.com/@{name})\n"
+    )
+    rss=scrape.getrss(name)
+    print(rss)
+    pyperclip.copy(rss)
